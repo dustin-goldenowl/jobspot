47ccc915eaae864e62dadc44461bd703d4c4b109
Trần Ngọc Tiến <144195318+WalterGoldenOwl@users.noreply.github.com>
2023-10-10 10:42:46 +0700

    fix conflict

--- lib/src/presentations/add_job/domain/router/add_job_coordinator.dart
+++ lib/src/presentations/add_job/domain/router/add_job_coordinator.dart
@@ -1,1 +1,15 @@
+import 'package:jobspot/injection.dart';
+import 'package:jobspot/src/core/config/router/app_router.dart';
+import 'package:jobspot/src/core/config/router/app_router.gr.dart';
 
+class AddJobCoordinator {
+  AddJobCoordinator._();
+
+  static AppRouter get rootRouter => getIt<AppRouter>();
+
+  static void showJobLocation({required Function(int code) onBack}) =>
+      rootRouter.push(LocationRoute(onBack: onBack));
+
+  static void showJobPosition({required Function(String title) onBack}) =>
+      rootRouter.push(JobPositionRoute(onBack: onBack));
+}
